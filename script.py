6a909b29b673afeea9cd4b42b5e61bf9607796aa
Matthieu Brillaxis <matthieubrillaxis@MacBook-Pro-de-Matthieu.local>
2018-04-20 16:22:28 +0200

    first commit

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 
-# chargement des données
+# chargement des donnÃ©es
 train = pd.read_csv('data/train.csv')
 test = pd.read_csv('data/test.csv')
 
@@ -15,10 +15,10 @@ test['Sex'] = pd.get_dummies(test['Sex'], drop_first=True)
 y = train['Survived']
 X = train[['Age', 'Sex']]
 
-# séparation en datasets de train et de test
+# sÃ©paration en datasets de train et de test
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-# entrainement d'une régression linéaire
+# entrainement d'une rÃ©gression linÃ©aire
 model = LogisticRegression()
 model.fit(X_train, y_train)
 model.score(X_test, y_test)
